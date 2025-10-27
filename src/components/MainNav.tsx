e6b35d36a98a972239c15409d92c79b885d10834
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-27 06:26:59 +0330

    Add main nav

---
+++ src/components/MainNav.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/Avatar";
+
+export default function MainNav() {
+  return (
+    <header className="flex justify-between px-4 py-3">
+      <span className="text-xl">Logo</span>
+
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </header>
+  );
+}
