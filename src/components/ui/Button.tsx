83462ac04213126f5090c9080ba788588377b240
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-27 09:48:46 +0330

    Update button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -19,9 +19,9 @@ const buttonVariants = cva(
       },
 
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md px-3 text-xs has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-8 has-[>svg]:px-4",
+        default: "h-9 px-4 py-2 has-[>svg]:px-3 has-[>img]:px-3",
+        sm: "h-8 rounded-md px-3 text-xs has-[>svg]:px-2.5 has-[>img]:px-2.5",
+        lg: "h-10 rounded-md px-8 has-[>svg]:px-4 has-[>img]:px-4",
         icon: "h-9 w-9",
       },
     },
