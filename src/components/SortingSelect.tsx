bdaa3a7e6a067c23e25c2ad500f3e42b2eaef894
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-28 17:05:37 +0330

    Update select sorting styles

--- src/components/SortingSelect.tsx
+++ src/components/SortingSelect.tsx
@@ -12,8 +12,13 @@ import {
 import Image from "next/image";
 import React from "react";
 import HamburgerIcon from "../assets/icons/hamburger.svg";
+import { cn } from "@/lib/utils";
 
-export default function SortingSelect() {
+export default function SortingSelect({
+  triggerClassName,
+}: {
+  triggerClassName?: string;
+}) {
   const [selected, setSelected] = React.useState<string | undefined>(undefined);
 
   function handleChange(value: string) {
@@ -24,13 +29,18 @@ export default function SortingSelect() {
 
   return (
     <Select defaultValue={selected} onValueChange={handleChange}>
-      <SelectTrigger className="w-[180px] rounded-full border-gray-300 ring-offset-2 focus-visible:ring-gray-100">
+      <SelectTrigger
+        className={cn(
+          "w-[180px] rounded-full border-gray-300 ring-offset-2 focus-visible:ring-gray-100 py-2.5 text-xs sm:text-sm",
+          triggerClassName,
+        )}
+      >
         <div className="flex items-center gap-1">
           <Image src={HamburgerIcon} alt="Arrow Down Icon" />
           <SelectValue placeholder="Sort By" />
         </div>
       </SelectTrigger>
-      <SelectContent className="border-gray-300">
+      <SelectContent className="border-gray-300 bg-white">
         <SelectGroup>
           <SelectItem
             value="most-recent"
