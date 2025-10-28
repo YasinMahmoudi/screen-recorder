acbd212c637264cfa5e8f816cc19ea1b9036e34a
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-28 11:55:47 +0330

    Update search icon width and height

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 export default function Search({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center", className)}>
-      <Image src={Icon} alt="Search Icon" preload className="relative -right-0.5 w-5 h-5 aspect-square" />
+      <Image src={Icon} alt="Search Icon" width={20} height={20} className="relative -right-0.5 w-5 h-5 aspect-square" />
       <Input
         className="-ml-6 w-full px-8"
         placeholder="Search for people,tags,folders,spacesand Looms"
