087d35455da0487f3ec974eed9d87bcc4235f37d
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-28 08:48:47 +0330

    Add test page

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/Avatar";
+import Link from "next/link";
 
 export default function MainNav() {
   return (
-    <header className="flex justify-between border-b border-b-gray-200 h-16 px-4 py-3">
+    <header className="flex h-16 justify-between border-b border-b-gray-200 px-4 py-3">
       <span className="text-xl">Logo</span>
+
+      <div className="border border-none shadow bg-white self-center rounded-sm p-1 ">
+        <Link
+          href="/test"
+          className="bg-linear-90 from-violet-700 to-indigo-600 text-white block text-sm p-1 rounded-sm"
+        >
+          {" "}
+          Test Page{" "}
+        </Link>
+      </div>
 
       <Avatar>
         <AvatarImage src="https://github.com/shadcn.png" />
