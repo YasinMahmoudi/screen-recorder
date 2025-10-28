29331769a9f2922d87c346010130531bcfde3cdc
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-28 17:07:10 +0330

    Add filter and search header

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import Search from "@/components/search";
+import { cn } from "@/lib/utils";
+import SortingSelect from "@/components/SortingSelect";
+
+export default function Header({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn("my-4 flex items-center justify-between py-2 gap-2", className)}
+    >
+      <Search className="flex-2 sm:w-sm sm:flex-initial" />
+
+      <SortingSelect triggerClassName="flex-[.75] sm:w-[180px] sm:flex-initial " />
+    </div>
+  );
+}
