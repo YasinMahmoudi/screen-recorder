47b4a25c70bf30aa9f264cb689bfdd8f1998046e
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-29 10:45:42 +0330

    Add home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,23 @@
+import Header from "@/components/header";
+import TopHeader from "./_components/top-header";
+import VideoList from "@/components/VideoList";
+import Divider from "@/components/ui/Divider";
+import Pagination from "@/components/ui/Pagination";
+
+export default function Home() {
+  return (
+    <main className="mx-auto my-4 max-w-7xl px-4 md:px-8 py-2 md:py-4">
+      <TopHeader />
+
+      <Header />
+
+      <VideoList />
+
+      <Divider className="mt-8 mb-4"/>
+
+      <div className="flex justify-center p-2">
+        <Pagination />
+      </div>
+    </main>
+  );
+}
