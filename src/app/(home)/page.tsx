076f8d31c7236d553a9bb1f9e105b964ba640335
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-29 11:13:39 +0330

    Update home page top header

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/header";
-import TopHeader from "./_components/top-header";
+import TopHeader from "@/components/TopHeader";
 import VideoList from "@/components/VideoList";
 import Divider from "@/components/ui/Divider";
 import Pagination from "@/components/ui/Pagination";
@@ -7,7 +7,11 @@ import Pagination from "@/components/ui/Pagination";
 export default function Home() {
   return (
     <main className="mx-auto my-4 max-w-7xl px-4 md:px-8 py-2 md:py-4">
-      <TopHeader />
+      <TopHeader 
+      subTitle="Public Assets"
+      title="All Videos"
+      
+      />
 
       <Header />
 
