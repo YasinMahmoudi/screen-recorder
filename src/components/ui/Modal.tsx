4b0f6122e62e518ce9b20b64e63ddd43f208551d
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-29 15:51:25 +0330

    Add modal component

---
+++ src/components/ui/Modal.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import React, {
+  cloneElement,
+  createContext,
+  isValidElement,
+  JSX,
+  PropsWithChildren,
+  ReactElement,
+  ReactNode,
+  useContext,
+  useState,
+} from "react";
+import Button from "./Button";
+import { motion } from "motion/react";
+import { createPortal } from "react-dom";
+import { useOutSideClick } from "@/hooks/useOutSideClick";
+
+interface ModalContextProps {
+  openModalId: string | null;
+  setOpenModalId: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+const ModalContext = createContext<ModalContextProps>({
+  openModalId: null,
+  setOpenModalId: () => {},
+});
+
+export default function Modal({ children }: { children: ReactNode }) {
+  const [openModalId, setOpenModalId] = useState<string | null>(null);
+
+  return (
+    <ModalContext.Provider value={{ openModalId, setOpenModalId }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+function ModalTrigger({
+  children,
+  openId,
+}: {
+  openId: string;
+  children: ReactNode;
+}) {
+  const { setOpenModalId } = useModal();
+
+  if (isValidElement(children))
+    return cloneElement(children as JSX.Element, {
+      onClick: () => setOpenModalId(openId),
+    });
+
+  return cloneElement(<div>{children}</div>, {
+    onClick: () => setOpenModalId(openId),
+  });
+}
+
+function ModalContent({
+  children,
+  openId,
+}: {
+  children: ReactNode;
+  openId: string;
+}) {
+  const { openModalId, setOpenModalId } = useModal();
+
+  const ref = useOutSideClick(setOpenModalId);
+
+  if (openModalId !== openId) return null;
+
+  return createPortal(
+    <div className="fixed top-0 left-0 grid h-full w-full place-items-center bg-gray-950/30">
+      <motion.div
+        ref={ref}
+        initial={{ scale: 0.9 }}
+        animate={{ scale: 1, transition: { duration: 0.1 } }}
+        className="relative min-h-48 w-[min(90%,480px)] rounded-lg bg-white p-6 shadow"
+      >
+        <CloseModal />
+
+        <div>{children}</div>
+      </motion.div>
+    </div>,
+    document.body,
+  );
+}
+
+function CloseModal() {
+  const { setOpenModalId } = useModal();
+
+  return (
+    <div className="mb-2 p-1 text-right">
+      <Button
+        onClick={() => setOpenModalId(null)}
+        className="size-6 cursor-pointer bg-rose-50 text-rose-500"
+        size="icon"
+      >
+        X
+      </Button>
+    </div>
+  );
+}
+
+function useModal() {
+  const context = useContext<ModalContextProps>(ModalContext);
+
+  if (context === undefined) {
+    throw new Error("useModal must be used within a Modal");
+  }
+
+  return context;
+}
+
+Modal.Trigger = ModalTrigger;
+Modal.Content = ModalContent;
