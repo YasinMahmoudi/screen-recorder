fca07e4eb89e69124b7f4d449a27df5a306a4dce
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-29 11:13:12 +0330

    Update top header

---
+++ src/components/TopHeader.tsx
@@ -0,0 +1,58 @@
+import Button from "@/components/ui/Button";
+import Image from "next/image";
+import UploadIcon from "../assets/icons/upload.svg";
+import Recordicon from "../assets/icons/record.svg";
+
+interface TopHeaderProps {
+  imageSrc?: string;
+  subTitle: string;
+  title: string;
+}
+
+export default function TopHeader({
+  imageSrc,
+  subTitle,
+  title,
+}: TopHeaderProps) {
+  return (
+    <div className="flex flex-wrap items-center justify-between">
+      <figure className="flex items-center gap-2.5">
+        {imageSrc && (
+          <Image
+            src="/images/avatar.jpg"
+            alt="Avatar"
+            width={60}
+            height={60}
+            className="rounded-full"
+          />
+        )}
+
+        <div className="space-y-2">
+          <span className="text-[10px] font-medium text-gray-400 sm:text-sm">
+            {" "}
+            {subTitle}{" "}
+          </span>
+          <h3 className="text-base font-bold text-gray-900 capitalize sm:text-3xl">
+            {" "}
+            {title}{" "}
+          </h3>
+        </div>
+      </figure>
+
+      <div className="flex gap-2 sm:gap-4">
+        <Button
+          variant="outline"
+          className="h-10 w-10 cursor-pointer rounded-full border-gray-300 hover:border-transparent hover:bg-gray-50 sm:h-auto sm:w-auto"
+        >
+          <Image src={UploadIcon} alt="Upload Icon" />
+          <span className="hidden sm:block">Upload a video</span>
+        </Button>
+
+        <Button className="h-10 w-10 cursor-pointer rounded-full bg-violet-600 text-pink-50 hover:bg-violet-500 sm:h-auto sm:w-auto">
+          <Image src={Recordicon} alt="Video Icon" />
+          <span className="hidden sm:block">Record a video</span>
+        </Button>
+      </div>
+    </div>
+  );
+}
