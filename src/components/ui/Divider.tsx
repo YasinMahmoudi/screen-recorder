7cac4ad50afc1bc0f3231f88ea2c42f2458fc632
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-30 00:20:23 +0330

    Update divider component

--- src/components/ui/Divider.tsx
+++ src/components/ui/Divider.tsx
@@ -1,6 +1,14 @@
 import { cn } from "@/lib/utils";
 import React from "react";
 
-export default function Divider({ className }: { className?: string }) {
-  return <hr className={cn(", h-px w-full border-gray-200 my-2", className)} />;
+export default function Divider({
+  className,
+  mode = 'horizontal',
+}: {
+  className?: string;
+  mode?: "vertical" | "horizontal";
+}) {
+  const styles = mode === "horizontal" ? "my-2 h-px w-full" : "h-6 border-t-0 w-px";
+
+  return <hr className={cn(`${styles} bg-gray-200`, className)} />;
 }
