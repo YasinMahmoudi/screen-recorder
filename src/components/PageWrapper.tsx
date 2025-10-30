75c9e31dedade9e95e016c667a92dfeb0d3c4e5d
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-30 14:49:22 +0330

    Update page wrapper component

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,12 +1,20 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 export default function PageWrapper({
   children,
+  className = "",
 }: {
   children: React.ReactNode;
+  className: string;
 }) {
   return (
-    <main className="mx-auto my-4 max-w-7xl px-4 py-2 md:px-8 md:py-4">
+    <main
+      className={cn(
+        "mx-auto my-4 max-w-7xl px-4 py-2 md:px-8 md:py-4",
+        className,
+      )}
+    >
       {children}
     </main>
   );
