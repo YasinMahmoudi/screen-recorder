24ab2780cf9897c2f165a4dff823cc4a2cb8f68c
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-30 22:22:47 +0330

    Add sign in form

---
+++ src/components/SignInForm.tsx
@@ -0,0 +1,23 @@
+import Button from "@/components/ui/Button";
+import GoogleIcon from "@/assets/icons/google.svg";
+import Image from "next/image";
+
+export default function SignInForm() {
+  return (
+    <div className="mx-auto max-w-sm self-center rounded-2xl border border-gray-200 px-6 py-8 text-center shadow">
+      <h1 className="text-xl font-bold tracking-[-.8px] md:text-2xl">
+        {" "}
+        Welcome to our app{" "}
+      </h1>
+      <p className="mt-1 text-xs tracking-[-.4px] text-gray-400 md:text-sm">
+        Start creating and sharing your first video in a minute
+      </p>
+
+      <Button className="mt-6 w-full cursor-pointer rounded-full border border-gray-200 bg-white">
+        <Image src={GoogleIcon} alt="Google Icon" width={15} height={15} />
+
+        <span> Sign in with Google </span>
+      </Button>
+    </div>
+  );
+}
