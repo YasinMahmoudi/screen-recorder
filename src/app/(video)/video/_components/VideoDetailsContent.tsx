8b39516eabc50ff8e9425956e846ead144513621
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-30 19:39:23 +0330

    Update video details content component

--- src/app/(video)/video/_components/VideoDetailsContent.tsx
+++ src/app/(video)/video/_components/VideoDetailsContent.tsx
@@ -1,13 +1,10 @@
-import React from "react";
 import VideoDetailsInfo from "./VideoDetailsInfo";
-import Image from "next/image";
+import VideoPalyer from "./VideoPalyer";
 
 export default function VideoDetailsContent() {
   return (
     <div className="grid grid-cols-1 gap-x-8 gap-y-10 xmd:grid-cols-2 xmd:gap-y-0">
-      <div className="w-full aspect-video rounded-xl"> 
-      <Image src='/images/thambnail.jpg' width={652} height={360} quality={60} alt="Details Cover" className="w-full h-full rounded-xl object-cover" />  
-      </div>
+      <VideoPalyer/>
 
       <VideoDetailsInfo />
     </div>
