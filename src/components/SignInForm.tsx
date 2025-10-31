9df40e43a0fdc7d4001157f2e4f2013eeb5d54d6
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-31 17:07:18 +0330

    Add signin form clip path

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -4,26 +4,28 @@ import Image from "next/image";
 
 export default function SignInForm() {
   return (
-    <div className="mx-auto max-w-sm self-center rounded-2xl border border-gray-200 px-6 py-8 text-center shadow">
-      <h1 className="text-xl font-bold tracking-[-.8px] md:text-2xl">
-        {" "}
-        Welcome to our app{" "}
-      </h1>
-      <p className="mt-1 text-xs tracking-[-.4px] text-gray-400 md:text-sm">
-        Start creating and sharing your first video in a minute
-      </p>
+    <div className="clip-path grid place-items-center">
+      <div className="mx-auto max-w-sm rounded-2xl border border-gray-200 px-6 py-8 text-center shadow bg-white">
+        <h1 className="text-xl font-bold tracking-[-.8px] md:text-2xl">
+          {" "}
+          Welcome to our app{" "}
+        </h1>
+        <p className="mt-1 text-xs tracking-[-.4px] text-gray-400 md:text-sm">
+          Start creating and sharing your first video in a minute
+        </p>
 
-      <Button className="mt-6 w-full cursor-pointer rounded-full border border-gray-200 bg-white">
-        <Image
-          src={GoogleIcon}
-          alt="Google Icon"
-          width={30}
-          height={30}
-          className="h-5 w-5"
-        />
+        <Button className="mt-6 w-full cursor-pointer rounded-full border border-gray-200 bg-white">
+          <Image
+            src={GoogleIcon}
+            alt="Google Icon"
+            width={30}
+            height={30}
+            className="h-5 w-5"
+          />
 
-        <span> Sign in with Google </span>
-      </Button>
+          <span> Sign in with Google </span>
+        </Button>
+      </div>
     </div>
   );
 }
