f09c5c6d4a0a20143c09f777afa20997db7d7118
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-31 15:35:34 +0330

    Add AuthImage component

---
+++ src/app/(auth)/_components/AuthImage.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function AuthImage() {
+  return (
+    <figure className="relative hidden md:block">
+      <Image
+        src="/images/auth-image.png"
+        alt="Auth Image"
+        fill
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        loading="eager"
+        className="object-contain"
+      />
+    </figure>
+  );
+}
