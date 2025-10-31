6f0434704af7dc053bb1815bc6ec7a8158fbc948
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-31 17:15:15 +0330

    Add auth header

---
+++ src/app/(auth)/_components/AuthHeader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AuthHeader() {
+  return (
+    <header className="flex h-12 items-center justify-between border-b border-b-gray-200 px-4 py-3">
+      LOGO
+    </header>
+  );
+}
