ce4f7bdba99a81f7eb56387e2de2eed4b298d39d
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-31 16:06:30 +0330

    Update auth image

--- src/app/(auth)/_components/AuthImage.tsx
+++ src/app/(auth)/_components/AuthImage.tsx
@@ -4,12 +4,11 @@ export default function AuthImage() {
   return (
     <figure className="relative hidden md:block">
       <Image
-        src="/images/auth-image.png"
+        src="/images/auth-image.svg"
         alt="Auth Image"
         fill
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         loading="eager"
-        className="object-contain"
       />
     </figure>
   );
