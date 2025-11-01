3c1d7286f88116c6e2bf52c6248e281573d34d84
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-11-01 23:07:25 +0330

    Add logo in main nav

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import Logo from "@/components/Logo";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/Avatar";
 import Link from "next/link";
 
 export default function MainNav() {
   return (
-    <header className="flex h-16 justify-between border-b border-b-gray-200 px-4 py-3">
-      <span className="text-xl">Logo</span>
+    <header className="flex items-center justify-between border-b border-b-gray-200 px-4 py-3">
+      <Logo />
 
-      <div className="border border-none shadow bg-white self-center rounded-sm p-1 ">
+      <div className="self-center rounded-sm border border-none bg-white p-1 shadow">
         <Link
           href="/test"
-          className="bg-linear-90 from-violet-700 to-indigo-600 text-white block text-sm p-1 rounded-sm"
+          className="block rounded-sm bg-linear-90 from-violet-700 to-indigo-600 p-1 text-sm text-white"
         >
           {" "}
           Test Page{" "}
