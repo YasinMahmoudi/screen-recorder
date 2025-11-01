51c7cfeb6f08d7f42bd7a9a4b9d33d987bbddfd0
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-11-01 23:06:01 +0330

    Update logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,16 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Logo() {
+export default function Logo({ className = "" }: { className?: string }) {
   return (
-    <Link href="/" className="flex items-center gap-1">
+    <Link href="/">
       <Image
         src="/logo.png"
         alt="Logo Image"
         width={2000}
         height={2000}
-        className="h-12 w-12"
+        className={cn("h-12 w-12", className)}
       />
     </Link>
   );
