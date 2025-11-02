de1b6ac160ada29e1cccd9de23f23fd34b322c1e
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-11-03 01:00:57 +0330

    Add database cnfig in better auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,11 @@
+import { db } from "@/drizzle/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { schema } from "@/drizzle/schema";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+});
