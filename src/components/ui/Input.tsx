974baa3ad3bca60b3b17a6f1523d0d0f2c6c815c
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-11-03 14:51:08 +0330

    Update input component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -9,7 +9,7 @@ export default function Input({ className, ...props }: InputProps) {
   return (
     <input
       className={cn(
-        "rounded-full border border-gray-300 px-3 py-1.5 text-xs w-60 font-normal text-gray-700 outline-0 placeholder:text-[10px] placeholder:text-gray-400 placeholder:font-extralight focus-visible:ring focus-visible:ring-gray-100 focus-visible:ring-offset-1 sm:text-sm sm:placeholder:text-sm",
+        "rounded-full border border-gray-300 px-3 py-1.5 text-xs w-60 font-normal text-gray-700 outline-0 placeholder:text-[10px] placeholder:text-gray-400 placeholder:font-normal focus-visible:ring focus-visible:ring-gray-100 focus-visible:ring-offset-1 sm:text-sm sm:placeholder:text-xs",
         className,
       )}
       placeholder="Search anything..."
