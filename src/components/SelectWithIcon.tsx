e0e944bde4debb5d98b1e82b57383a1b4709bf74
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-11-04 13:44:31 +0330

    Update select with icon component by react hook form props

--- src/components/SelectWithIcon.tsx
+++ src/components/SelectWithIcon.tsx
@@ -11,7 +11,7 @@ import {
 
 import { cn } from "@/lib/utils";
 import Image from "next/image";
-import React from "react";
+import React, { ComponentPropsWithRef } from "react";
 
 interface SelectItemType {
   icon?: string;
@@ -19,37 +19,47 @@ interface SelectItemType {
   value: string;
 }
 
-interface SelectProps {
+type SelectProps = {
   selectLabel?: string;
   triggerClassName?: string;
   iconSrc?: string;
   items: SelectItemType[];
-  id?:string
-}
+  id?: string;
+  selectTriggerRef?: React.RefObject<HTMLButtonElement>;
+} & ComponentPropsWithRef<"select">;
 
 export default function SelectWithIcon({
   selectLabel = "Select on option",
   triggerClassName,
   iconSrc,
   items,
-  id
+  id,
+  selectTriggerRef,
+  ...props
 }: SelectProps) {
-  const [selected, setSelected] = React.useState<string | undefined>(undefined);
+  const { value, onChange, name } = props;
+
+  const [selected, setSelected] = React.useState<string | undefined>(
+    (value as string) ?? undefined,
+  );
 
   function handleChange(value: string) {
     setSelected(value);
+
+    if (onChange) onChange(value);
   }
 
   const selectedItemClassNames = "bg-violet-500 text-white";
 
   return (
-    <Select defaultValue={selected} onValueChange={handleChange}>
+    <Select defaultValue={selected} onValueChange={handleChange} name={name}>
       <SelectTrigger
         id={id}
         className={cn(
           "w-[180px] rounded-full border-gray-300 py-2.5 text-xs ring-offset-2 focus-visible:ring-gray-100 sm:text-sm",
           triggerClassName,
         )}
+        ref={selectTriggerRef}
       >
         <div className="flex items-center gap-1">
           {iconSrc && <Image src={iconSrc} alt="Icon Button" />}
