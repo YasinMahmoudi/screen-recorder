01f7b47a7535db53d6526d6c1695bfe4c727b4af
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-11-04 08:20:42 +0330

    Add bunny constants

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,7 @@
+export const BUNNY = {
+  STREAM_BASE_URL: "https://video.bunnycdn.com/library",
+  STORAGE_BASE_URL: "https://storage.bunnycdn.com/capterra",
+  CDN_URL: "https://capterra.b-cdn.net",
+  EMBED_URL: "https://iframe.mediadelivery.net/embed",
+  TRANSCRIPT_URL: "https://vz-889fd4a3-005.b-cdn.net",
+};
