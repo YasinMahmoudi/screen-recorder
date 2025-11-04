b1be2ad286cd9fb10adbf579dc79394a2a644183
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-11-04 17:47:59 +0330

    Update video list and video card components

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -4,41 +4,52 @@ import Image from "next/image";
 import ArrowDownIcon from "../assets/icons/arrow-down.svg";
 import EyeIcon from "../assets/icons/eye.svg";
 
+import CopyLinkButton from "@/components/CopyLinkButton";
 import Link from "next/link";
 import React from "react";
-import CopyLinkButton from "@/components/CopyLinkButton";
 
 export type VideoCardProps = {
-  id: string;
-  title: string;
-  description?: string;
-  thumbnail: string;
-  createdAt: string;
-  author: {
-    name: string;
-    image: string;
+  video: {
+    id: string;
+    title: string;
+    description?: string;
+    thumbnailUrl: string;
+    createdAt: Date;
+
+    views: number;
+    duration: number | null;
   };
-  views: number;
-  duration: number;
+
+  user: {
+    id: string;
+    name: string;
+    image: string | null;
+  } | null;
 };
 
-export default function VideoCard({ video }: { video: VideoCardProps }) {
+export default function VideoCard({
+  video: { video, user },
+}: {
+  video: VideoCardProps;
+}) {
   return (
     <div className="aspect-video w-full rounded-2xl border border-gray-300 pb-6">
       <div className="relative h-40">
         <Image
-          width={100}
-          height={100}
-          src={video.thumbnail}
+          width={700}
+          height={240}
+          src={video.thumbnailUrl}
           alt={video.title}
           priority
           className="h-full w-full rounded-t-2xl object-cover"
         />
 
-        <span className="absolute right-2.5 bottom-4 rounded-full bg-gray-900 px-2.5 py-1 text-xs font-medium tracking-tighter text-gray-50">
-          {" "}
-          {video.duration} min{" "}
-        </span>
+        {video.duration && (
+          <span className="absolute right-2.5 bottom-4 rounded-full bg-gray-900 px-2.5 py-1 text-xs font-medium tracking-tighter text-gray-50">
+            {" "}
+            {video.duration} min{" "}
+          </span>
+        )}
 
         <VideoCardActions>
           <CopyLinkButton id={video.id} />
@@ -47,16 +58,21 @@ export default function VideoCard({ video }: { video: VideoCardProps }) {
 
       <div className="flex justify-between px-3 py-4">
         <div className="flex items-center gap-2">
-          <Image
-            width={30}
-            height={30}
-            src={video.author.image}
-            alt={video.author.name}
-            className="h-[34px] w-[34px] rounded-full"
-          />
+          {user && (
+            <Image
+              width={30}
+              height={30}
+              src={user.image!}
+              alt={user.name}
+              className="h-[34px] w-[34px] rounded-full"
+            />
+          )}
+
           <div className="space-y-0.5">
             <div className="flex items-center gap-1.5">
-              <span className="text-xs font-medium"> {video.author.name}</span>
+              {user && (
+                <span className="text-xs font-medium"> {user.name}</span>
+              )}
               <div className="h-1 w-1 rounded-full bg-gray-500"></div>
               <span className="text-xs text-gray-400"> 2mo</span>
             </div>
@@ -80,28 +96,40 @@ export default function VideoCard({ video }: { video: VideoCardProps }) {
         </div>
       </div>
 
-      <VideoCardTitle video={video} />
+      <VideoCardTitle
+        id={video.id}
+        title={video.title}
+        date={video.createdAt}
+      />
     </div>
   );
 }
 
-function VideoCardTitle({ video }: { video: VideoCardProps }) {
-  const title = `${video.title} ${String.fromCharCode(8212)} ${new Intl.DateTimeFormat(
+function VideoCardTitle({
+  title,
+  date,
+  id,
+}: {
+  title: string;
+  date: Date;
+  id: string;
+}) {
+  const videoTitle = `${title} ${String.fromCharCode(8212)} ${new Intl.DateTimeFormat(
     "en-US",
     {
       year: "numeric",
       month: "short",
       day: "numeric",
     },
-  ).format(new Date(video.createdAt))}
+  ).format(new Date(date))}
     `;
 
   return (
     <h1
       className="mr-2 ml-3.5 line-clamp-2 font-semibold tracking-[-0.8px] text-gray-900 hover:text-gray-700"
-      title={title}
+      title={videoTitle}
     >
-      <Link href={`/video/${video.id}`}>{title}</Link>
+      <Link href={`/video/${id}`}>{videoTitle}</Link>
     </h1>
   );
 }
@@ -109,4 +137,3 @@ function VideoCardTitle({ video }: { video: VideoCardProps }) {
 function VideoCardActions({ children }: { children: React.ReactNode }) {
   return <div className="absolute top-4 right-2.5">{children}</div>;
 }
-
--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -1,96 +1,17 @@
 import VideoCard, { type VideoCardProps } from "@/components/VideoCard";
+import EmptyState from "./EmptyState";
 
-const videos: VideoCardProps[] = [
-  {
-    id: "1",
-    title: "Video Title For Testing Purposes Only Please Ignore",
-    description: "Video Description",
-    thumbnail: "/images/thambnail.jpg",
-    createdAt: "2022-01-01",
-    author: {
-      name: "John Doe",
-      image: "/images/avatar.jpg",
-    },
-    views: 100,
-    duration: 60,
-  },
+export default function VideoList({
+  videos = [],
+}: {
+  videos: VideoCardProps[];
+}) {
+  if (videos.length === 0) return <EmptyState />;
 
-  {
-    id: "2",
-    title: "Video Title For Testing Purposes Only Please Ignore",
-    description: "Video Description",
-    thumbnail: "/images/thambnail.jpg",
-    createdAt: "2022-01-01",
-    author: {
-      name: "John Doe",
-      image: "/images/avatar.jpg",
-    },
-    views: 100,
-    duration: 60,
-  },
-
-  {
-    id: "3",
-    title: "Video Title For Testing Purposes Only Please Ignore",
-    description: "Video Description",
-    thumbnail: "/images/thambnail.jpg",
-    createdAt: "2022-01-01",
-    author: {
-      name: "John Doe",
-      image: "/images/avatar.jpg",
-    },
-    views: 100,
-    duration: 60,
-  },
-
-  {
-    id: "4",
-    title: "Video Title For Testing Purposes Only Please Ignore",
-    description: "Video Description",
-    thumbnail: "/images/thambnail.jpg",
-    createdAt: "2022-01-01",
-    author: {
-      name: "John Doe",
-      image: "/images/avatar.jpg",
-    },
-    views: 100,
-    duration: 60,
-  },
-
-  {
-    id: "5",
-    title: "Video Title For Testing Purposes Only Please Ignore",
-    description: "Video Description",
-    thumbnail: "/images/thambnail.jpg",
-    createdAt: "2022-01-01",
-    author: {
-      name: "John Doe",
-      image: "/images/avatar.jpg",
-    },
-    views: 100,
-    duration: 60,
-  },
-
-  {
-    id: "6",
-    title: "Video Title For Testing Purposes Only Please Ignore",
-    description: "Video Description",
-    thumbnail: "/images/thambnail.jpg",
-    createdAt: "2022-01-01",
-    author: {
-      name: "John Doe",
-      image: "/images/avatar.jpg",
-    },
-    views: 100,
-    duration: 60,
-  },
-];
-
-export default function VideoList() {
   return (
-    <div className="sm:mt-10 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-8">
-      {videos.map((video) => (
-        <VideoCard key={video.id} video={video} />
+    <div className="xs:grid-cols-2 grid grid-cols-1 gap-x-6 gap-y-8 sm:mt-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {videos.map((item: VideoCardProps) => (
+        <VideoCard key={item.video.id} video={item} />
       ))}
     </div>
   );
