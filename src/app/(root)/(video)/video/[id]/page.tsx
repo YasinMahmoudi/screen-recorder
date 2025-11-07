5eeb00539906f7e8c590d01f6dfeb2d89df27fa4
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-11-07 22:33:56 +0330

    Add video details not found error

--- src/app/(root)/(video)/video/[id]/page.tsx
+++ src/app/(root)/(video)/video/[id]/page.tsx
@@ -2,6 +2,7 @@ import PageWrapper from "@/components/PageWrapper";
 import VideoDetailsHeader from "../_components/VideoDetailsHeader";
 import VideoDetailsContent from "../_components/VideoDetailsContent";
 import { getVideo } from "@/lib/actions/video";
+import { notFound } from "next/navigation";
 
 export default async function Page({
   params,
@@ -11,6 +12,10 @@ export default async function Page({
   const videoId = (await params).id;
 
   const videoDetails = await getVideo(videoId);
+
+  if (!videoDetails) {
+    return notFound();
+  }
 
   return (
     <PageWrapper className="space-y-16">
