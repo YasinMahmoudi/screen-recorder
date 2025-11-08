d152086a52b455e06c3571a0242f18f2f30c5c4d
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-11-08 21:36:27 +0330

    Update drizzle db

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,8 +1,5 @@
-import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-
-config({ path: "../../.env" });
 
 const postgress = neon(process.env.POSTGRES_URL!);
 
