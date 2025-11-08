b53ad298636de594070f13c2b9fba21e2d794756
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-11-08 15:03:55 +0330

    Remove test page

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -7,16 +7,6 @@ export default function MainNav() {
     <header className="flex items-center justify-between border-b border-b-gray-200 px-4 py-3">
       <Logo />
 
-      <div className="self-center rounded-sm border border-none bg-white p-1 shadow">
-        <Link
-          href="/test"
-          className="block rounded-sm bg-linear-90 from-violet-700 to-indigo-600 p-1 text-sm text-white"
-        >
-          {" "}
-          Test Page{" "}
-        </Link>
-      </div>
-
       <UserAvatar />
     </header>
   );
